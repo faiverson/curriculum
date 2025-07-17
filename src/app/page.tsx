80eced3d026a97a3fc9162bbe70a55e589943a16
Fabian Torres <fabian.torres@schreiberfoods.com>
2025-07-17 12:45:13 -0300

    hide icons on print

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         <header>
           <div className="box-border dark:bg-bold-black border-[0.1rem]-bold-black-alternative bg-neutral-100 h-12 print:pt-40">
             <div className='h-full flex flex-row-reverse justify-between'>
-              <button className='pr-2 dark:text-white text-bg-bold-black hover:scale-110 active:scale-100 duration-200 ' onClick={() => theme === 'dark' ? setTheme('light') : setTheme('dark')}>
+              <button className='pr-2 dark:text-white text-bg-bold-black hover:scale-110 active:scale-100 duration-200 print:hidden' onClick={() => theme === 'dark' ? setTheme('light') : setTheme('dark')}>
                 {theme === 'dark'
                   ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
--- src/components/Burger.tsx
+++ src/components/Burger.tsx
@@ -33,7 +33,7 @@ export default function Burger() {
 
   return (
     <>
-      <button className='pl-2 w-full dark:text-white text-bg-bold-black md:hidden' onClick={toggleBurger} aria-label="Toggle Navbar">
+      <button className='pl-2 w-full dark:text-white text-bg-bold-black md:hidden print:hidden' onClick={toggleBurger} aria-label="Toggle Navbar">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
